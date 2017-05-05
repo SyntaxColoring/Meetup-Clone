daaaf32efd89b364e05fe45f9b3637d6cbd2278d
Pypet16 <peter.smondyrev@gmail.com>
2017-05-04 23:37:42 -0400

    Add files via upload

---
+++ belongToGroup.php
@@ -0,0 +1,24 @@
+<?php
+	#code to use on webpage in which groups the user belongs to are shown
+	include("meetupConfig.php");
+	session_start();
+	
+	$stmt = $db->prepare("SELECT groups.group_name FROM groups INNER JOIN belongs_to
+						ON groups.group_id = belongs_to.groups_id 
+						WHERE belongs_to.username");
+	$userN = $_SESSION['login_user'];
+	$stmt->bind_param('s', $userN);
+	$stmt->execute();
+	
+	$result = $stmt->get_result();
+	
+	$stmt->close();
+	
+	while($row = $result->fetch_assoc()){
+		# print the $row[groups.group_name] which the user is affiliated with.
+	
+	}
+
+
+
+?>
