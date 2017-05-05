04794fac438cc8c7d00bbc8d2aa2a46c146e9f79
Pypet16 <peter.smondyrev@gmail.com>
2017-05-05 11:47:23 -0400

    Update interestGroup.php

--- interestGroup.php
+++ interestGroup.php
@@ -1,14 +1,29 @@
 <?php
+include("meetupConfig.php");
+session_start();
 	
-
-
-
-?>
-
-<?php
+	#Interest Picked and groups associated with that interest are shown
+	
 	if($_SERVER['REQUEST_METHOD'] == 'POST'){
-		if(isset($_POST['BACK'])){
-			#header('Location: back to some state')
+		if(isset($_POST['INTEREST'])){ #may want to take out since we getting interests of associated user
+			$interName = $_POST['INTEREST'];
+			$stat = $db->prepare("SELECT groups.group_name FROM about inner join groups 
+									ON about.group_id = groups.group_id
+									WHERE about.interest_name = ?");
+			
+			$stat->bind_param('s', $interName);
+			$stat->execute();
+			
+			$result = $stat->get_result();
+			$stat->close();
+			
+			#div tag echoed.  
+			while($row = $result->fetch_assoc()){
+			#group name displayed multiple times
+				echo $row['group_name'];
+			
+			}
+			#end of div tag
 			
 		}
 	
