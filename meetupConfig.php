739411ea4c65584806727c8a6aa279a6a0940ff2
Max Marrone <max@marrone.nyc>
2017-05-09 02:45:02 -0400

    Change DB name to "meetup" to match the name suggested in the example .sql file.

--- meetupConfig.php
+++ meetupConfig.php
@@ -2,7 +2,7 @@
 	define('dbserv', 'localhost:3306');
 	define('dbuser', 'root');
 	define('dbpass', 'root');
-    define('dabname', 'meetup2.0');
+    define('dabname', 'meetup');
 	$db = new mysqli(dbserv,dbuser,dbpass,dabname);
 	
 
