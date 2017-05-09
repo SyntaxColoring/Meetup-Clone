165a778ae21d562ee749a616a9a5f2ed3f566f39
Max Marrone <max@marrone.nyc>
2017-05-09 05:00:38 -0400

    Add user info to header.

--- header.php
+++ header.php
@@ -1,3 +1,11 @@
 <h1>Meetup</h1>
-<p><a href="login.php">Log in or register</a></p>
+<?php
+	if (isset($_SESSION['login_user'])) {
+		
+		echo '<p>Welcome, ' . $_SESSION['login_user'] . '. <a href="logout.php">Log out</a></p>';
+	}
+	else {
+		echo '<p><a href="login.php">Log in or register</a></p>';
+	}
+?>
 <p><a href="">Home</a></p>
