8cc030c7b3c929c7d1f6f229abc4ae05f415af1d
Max Marrone <max@marrone.nyc>
2017-05-09 13:16:28 -0400

    Implement RSVPs.

--- event.php
+++ event.php
@@ -15,12 +15,33 @@
 		$statement->bind_param('s', $id);
 		$statement->execute();
 		$result = $statement->get_result();
-		$row = $result->fetch_assoc();
 		
-		echo '<h2>Event: '.$row['title'].'</h2>';
-		echo '<p>Description: '.$row['description'].'</p>';
-		echo '<p>When: '.$row['start_time'].' to '.$row['end_time'].'</p>';
-		echo '<p>Where: '.$row['lname'].'</p>';
+		$row = $result->fetch_assoc();
+		if ($result->num_rows > 0) {
+			echo '<h2>Event: '.$row['title'].'</h2>';
+			echo '<p>Description: '.$row['description'].'</p>';
+			echo '<p>When: '.$row['start_time'].' to '.$row['end_time'].'</p>';
+			echo '<p>Where: '.$row['lname'].'</p>';
+			
+			if (isset($_SESSION['login_user'])) {
+				echo '<form action="rsvp.php" method="post">';
+				echo '<div><label>Going <input type="radio" name="rsvp" value="1"></label></div>';
+				echo '<div><label>Not going <input type="radio" name="rsvp" value="0"></label></div>';
+				echo '<input type="hidden" name="event_id" value="'.$id.'">';
+				echo '<input type="submit" value="RSVP">';
+				echo '</form>';
+				if (isset($_SESSION['rsvp_message'])) {
+					echo '<p>'.$_SESSION['rsvp_message'].'</p>';
+					unset($_SESSION['rsvp_message']);
+				}
+			}
+			else {
+				echo '<p>Log in to RSVP.</p>';
+			}
+		}
+		else {
+			echo '<p>Error: There\'s no event with that ID.</p>';
+		}
 		$result->free();
 		$statement->close();
 		$db->close();
--- rsvp.php
+++ rsvp.php
@@ -4,10 +4,9 @@
 #Redwanul Mutee, rm4243
 #Max Marrone, mpm507
 include("meetupConfig.php");
+session_start();
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-	$eventN = $_POST['eventName'];
-
 	$eID = $_POST['event_id'];
 	$postVal = $_POST['rsvp'];
 	$userN = $_SESSION['login_user'];
@@ -17,41 +16,32 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 	$sqlStat->execute();
 	$result = $sqlStat->get_result();
 	
-	$countRow = mysqli_num_rows($result)
+	$countRow = mysqli_num_rows($result);
 	
+	// If the user has RSVPd to this event in the past, we need to update that
+	// record instead of inserting a new one.
 	if($countRow > 0){
-		if($result['rsvp'] == 1){
-			$sqlStat->close();
-			$db->close;
-			header("Location: ");
-			exit;
-			
-		else{
-			$sqlStat->close();
-			$sqlStat = $db->prepare("UPDATE attend SET rsvp = 1 where username = ?  AND event_id = ?");
-			$sqlStat->bind_param('si', $userN, $eID);
-			$sqlStat->execute();
-			$sqlStat->close();
-			$db->close();
-			header("Location: ");
-			exit;
-		}
-		
-	}
-	
-	if(mysql_num_rows($result) == 0){
 		$sqlStat->close();
-		$sqlStat = $db->prepare("INSERT INTO attend(event_id, username, rsvp) values(?, ?, ?)");
-		$sqlStat->bind_param('isi', $eID, $userN, $postVal);
+		$sqlStat = $db->prepare("UPDATE attend SET rsvp = ? where username = ?  AND event_id = ?");
+		$sqlStat->bind_param('isi', $postVal, $userN, $eID);
+		$sqlStat->execute();
 		$sqlStat->close();
 		$db->close();
-		header("Location: ");
-		exit;
+		$_SESSION['rsvp_message'] = "RSVP updated.";
 	}
 	
+	else {
+		$rating = 0;
+		$sqlStat->close();
+		$sqlStat = $db->prepare("INSERT INTO attend(event_id, username, rsvp, rating) values(?, ?, ?, ?)");
+		$sqlStat->bind_param('isii', $eID, $userN, $postVal, $rating);
+		$sqlStat->execute();
+		$sqlStat->close();
+		$db->close();
+		$_SESSION['rsvp_message'] = "RSVPd!";
+	}
 	
+	header("Location: /event.php?id=$eID");
+	exit;
 }
-
-
-
 ?>
